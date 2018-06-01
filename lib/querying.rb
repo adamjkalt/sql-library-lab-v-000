b3eb6914369dc1d94f20e6aae61712d498b6e7b0
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-01 05:24:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,13 +28,14 @@ def select_value_and_count_of_most_prolific_species
    end
 
 def select_name_and_series_subgenres_of_authors
-    <<-SQL
-        SELECT value, COUNT(*)
-        FROM characters
-        GROUP BY species
-        ORDER BY COUNT(species) DESC
-        LIMIT 1
-      SQL
+  <<-SQL
+    SELECT authors.name, subgenres.name 
+    FROM authors 
+    JOIN series 
+    ON series.author_id = authors.id 
+    JOIN subgenres 
+    ON series.subgenre_id = subgenres.id
+  SQL
    end
 
 def select_series_title_with_most_human_characters
