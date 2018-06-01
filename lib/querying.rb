430989de87bd988a734abee000946a6b3aa56a51
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-01 05:13:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
 <<-SQL
-    "SELECT title, year
+    SELECT title, year
     FROM books
     WHERE series_id=1
-    ORDER BY year"
+    ORDER BY year
   SQL
 end
 
