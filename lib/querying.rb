3e38c27314c4ef4d2b08e819e983b9cb5b0d64fa
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-01 05:19:35 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,8 +18,13 @@ def select_name_and_motto_of_char_with_longest_motto
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
-end
+  <<-SQL
+      SELECT value, COUNT(*)
+      FROM species
+      ORDER BY LENGTH(motto) DESC
+      LIMIT 1
+    SQL
+   end
 
 def select_name_and_series_subgenres_of_authors
   "Write your SQL query here"
